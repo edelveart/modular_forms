588511018b61e902236feb9d125be607851767e0
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-20 00:36:53 -0500

    feat: add binomial_coefficient helper

--- lib/numeric_helpers/numeric_helpers.rb
+++ lib/numeric_helpers/numeric_helpers.rb
@@ -1,13 +1,19 @@
+# frozen_string_literal: true
 
+# NumericHelpers
+#
+# Contains functions for generating modular forms and performing
+# other numerical operations.
 module NumericHelpers
-
   def self.factorial_iter(num)
     t = 1
     (1..num).each do |i|
-      t = t * i
+      t *= i
     end
     t
   end
 
-
+  def self.binomial_coefficient(n, k) # rubocop:disable Naming/MethodParameterName
+    factorial_iter(n) / (factorial_iter(k) * factorial_iter(n - k))
+  end
 end
