5ad3d61d905afdb419e287c8b7efa08720b1c7b9
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-20 03:10:37 -0500

    refactor: move NumericHelpers module inside ModularForms

--- lib/numeric_helpers/numeric_helpers.rb
+++ lib/numeric_helpers/numeric_helpers.rb
@@ -1,42 +1,44 @@
 # frozen_string_literal: true
 
-# NumericHelpers
-#
-# Contains functions used in the definitions of modular forms,
-# as well as other related numerical operations.
-module NumericHelpers
-  def self.factorial_iter(num)
-    t = 1
-    (1..num).each do |i|
-      t *= i
-    end
-    t
-  end
-
-  def self.binomial_coefficient(n, k)
-    factorial_iter(n) / (factorial_iter(k) * factorial_iter(n - k))
-  end
-
-  def self.bernoulli_numbers_arr(n, bernoulli_numbers = [1])
-    return bernoulli_numbers[n] if n < bernoulli_numbers.length
-
-    (bernoulli_numbers.length..n).each do |k|
-      sum = Rational(0, 1)
-      (0...k).each do |j|
-        sum += binomial_coefficient(k + 1, j) * bernoulli_numbers[j]
+module ModularForms
+  # NumericHelpers
+  #
+  # Contains functions used in the definitions of modular forms,
+  # as well as other related numerical operations.
+  module NumericHelpers
+    def self.factorial_iter(num)
+      t = 1
+      (1..num).each do |i|
+        t *= i
       end
-      bernoulli_numbers[k] = -sum / (k + 1)
+      t
     end
-    bernoulli_numbers
-  end
 
-  def self.divisor_function_sigma(n, z)
-    total_sum = 0
-    (1..n).each do |d|
-      if n % d == 0 # rubocop:disable Style/NumericPredicate,Style/IfUnlessModifier
-        total_sum += d**z
-      end
+    def self.binomial_coefficient(n, k)
+      factorial_iter(n) / (factorial_iter(k) * factorial_iter(n - k))
     end
-    total_sum
+
+    def self.bernoulli_numbers_arr(n, bernoulli_numbers = [1])
+      return bernoulli_numbers[n] if n < bernoulli_numbers.length
+
+      (bernoulli_numbers.length..n).each do |k|
+        sum = Rational(0, 1)
+        (0...k).each do |j|
+          sum += binomial_coefficient(k + 1, j) * bernoulli_numbers[j]
+        end
+        bernoulli_numbers[k] = -sum / (k + 1)
+      end
+      bernoulli_numbers
+    end
+
+    def self.divisor_function_sigma(n, z)
+      total_sum = 0
+      (1..n).each do |d|
+        if n % d == 0 # rubocop:disable Style/NumericPredicate,Style/IfUnlessModifier
+          total_sum += d**z
+        end
+      end
+      total_sum
+    end
   end
 end
