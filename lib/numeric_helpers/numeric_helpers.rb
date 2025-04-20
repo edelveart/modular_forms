f9f5e8573e3a29a9ba1ff12731708b73c43397a6
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-20 00:42:35 -0500

    feat: add Bernoulli numbers implementation
    Added an implementation for calculating Bernoulli numbers. Further improvements are needed for more efficient handling of larger values.

--- lib/numeric_helpers/numeric_helpers.rb
+++ lib/numeric_helpers/numeric_helpers.rb
@@ -16,4 +16,17 @@ module NumericHelpers
   def self.binomial_coefficient(n, k) # rubocop:disable Naming/MethodParameterName
     factorial_iter(n) / (factorial_iter(k) * factorial_iter(n - k))
   end
+
+  def self.bernoulli_arr(n, bernoulli_numbers = [1]) # rubocop:disable Naming/MethodParameterName
+    return bernoulli_numbers[n] if n < bernoulli_numbers.length
+
+    (bernoulli_numbers.length..n).each do |k|
+      sum = Rational(0, 1)
+      (0...k).each do |j|
+        sum += binomial(k + 1, j) * bernoulli_numbers[j]
+      end
+      bernoulli_numbers[k] = -sum / (k + 1)
+    end
+    bernoulli_numbers
+  end
 end
