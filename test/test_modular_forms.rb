721666aa8b05d2ebdd055f34541cb25a5cef05b1
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-21 20:52:31 -0500

    test: add unit tests for eisenstein_series with gal_f parameter in ModularForms

--- test/test_modular_forms.rb
+++ test/test_modular_forms.rb
@@ -15,9 +15,23 @@ class TestModularForms < Minitest::Test
     10 => [1, -264, -135_432, -5_196_576, -69_341_448, -515_625_264, -2_665_843_488, -10_653_352_512, -35_502_821_640, -102_284_205_672, -264_515_760_432, -622_498_190_688, -1_364_917_062_432, -2_799_587_834_736, -5_465_169_838_656, -10_149_567_696_576, -18_177_444_679_944, -31_307_199_395_472, -52_471_797_509_736, -85_189_552_213_920] # rubocop:disable Layout/LineLength
   }.freeze
 
+  EISENSTEIN_SERIES_EXPECTED_GAL_F = {
+    6 => [1, 1, 3, 4, 2, 1, 2, 3, 0, 3, 3, 2, 3, 4, 4, 4, 1, 3, 4, 0],
+    8 => [1, 4, 6, 14, 7, 10, 4, 1, 16, 5, 15, 16, 16, 3, 10, 1, 12, 4, 16, 6],
+    114 => [1, 0, 0, 0, 0, 0, 0, 56, 50, 28, 66, 52, 43, 9, 83, 70, 16, 50, 29, 59]
+  }.freeze
+
   def test_eisenstein_series
     EISENSTEIN_SERIES_EXPECTED.each do |weight, expected|
       actual = ModularForms.eisenstein_series(weight).take(20)
+      assert_equal(expected, actual, "Failed for weight k = #{weight}")
+    end
+  end
+
+  def test_eisenstein_series_gal_f
+    gal_f_by_weight = { 6 => 5, 8 => 17, 114 => 97 }
+    EISENSTEIN_SERIES_EXPECTED_GAL_F.each do |weight, expected|
+      actual = ModularForms.eisenstein_series(weight, gal_f_by_weight[weight]).take(20)
       assert_equal(expected, actual, "Failed for weight k = #{weight}")
     end
   end
@@ -28,6 +42,16 @@ class TestModularForms < Minitest::Test
     invalid_values.each do |invalid_k|
       assert_raises(ArgumentError, "Expected error for invalid k = #{invalid_k}") do
         ModularForms.eisenstein_series(invalid_k).take(5)
+      end
+    end
+  end
+
+  def test_eisenstein_series_with_invalid_gal_f
+    invalid_gal_f = [-4, 1, 982, 12, 30, 6_897_942_012, 1134.3534]
+
+    invalid_gal_f.each do |invalid_val|
+      assert_raises(ArgumentError, "GF must be a prime number = #{invalid_val}") do
+        ModularForms.eisenstein_series(6, invalid_val).take(5)
       end
     end
   end
