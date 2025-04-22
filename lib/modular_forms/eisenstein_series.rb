20e5dd2dc93e06012ee6e8990b56e0ba7a7cd5eb
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-21 20:10:47 -0500

    refactor(EisensteinSeries): improve variable names, add Galois field calculation and new parameter in einsenstein_modular_form

--- lib/modular_forms/eisenstein_series.rb
+++ lib/modular_forms/eisenstein_series.rb
@@ -9,16 +9,20 @@ module ModularForms
   # that are invariant under modular group transformations. It can be used to
   # calculate coefficients and is suitable for applications in Sonic Pi.
   module EisensteinSeries
-    def self.eisenstein_modular_form(k)
+    def self.eisenstein_modular_form(k, gal_f = nil) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
       raise ArgumentError, 'k must be even and >= 2' if k < 2 || k % 2 != 0 # rubocop:disable Style/EvenOdd
 
       bernoulli_k = NumericHelpers.bernoulli_numbers_arr(k)[k]
-      factor_normalization = - Rational(2 * k, bernoulli_k)
+      normalization_factor = - Rational(2 * k, bernoulli_k)
       Enumerator.new do |q|
         q << 1
         (1..Float::INFINITY).each do |tau|
-          coeff = NumericHelpers.divisor_function_sigma(tau, k - 1)
-          q << (factor_normalization * coeff).to_i
+          nth_term = (normalization_factor * NumericHelpers.divisor_function_sigma(tau, k - 1)).to_i
+          if gal_f != nil # rubocop:disable Style/ConditionalAssignment,Style/NonNilCheck
+            q << NumericHelpers.galois_field(nth_term, gal_f)
+          else
+            q << nth_term
+          end
         end
       end
     end
