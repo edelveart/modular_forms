cf54d3149836d589068db303ba2326b2ae2da2c1
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-22 01:46:35 -0500

    refactor(DedekindEtaFunctions): rename boolean parameter of eta_function method

--- lib/modular_forms/dedekind_eta_functions.rb
+++ lib/modular_forms/dedekind_eta_functions.rb
@@ -5,7 +5,7 @@ module ModularForms
   #
   # This module provides a generator for Dedekind eta functions.
   module DedekindEtaFunctions
-    def self.eta_function(pentagonal_coefs = false) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Style/OptionalBooleanParameter
+    def self.eta_function(pent_coefs = false) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Style/OptionalBooleanParameter
       Enumerator.new do |q|
         q << 1
         (1..Float::INFINITY).each do |tau|
@@ -13,11 +13,11 @@ module ModularForms
           pentagonal_num_plus = (3 * tau**2 + tau) / 2
           min_next_pent = (3 * (tau + 1)**2 - (tau + 1)) / 2
           sign = (-1)**tau
-          q << (pentagonal_coefs == false ? sign : sign * pentagonal_num_minus)
+          q << (pent_coefs == false ? sign : sign * pentagonal_num_minus)
           (pentagonal_num_plus - pentagonal_num_minus - 1).abs.times do
             q << 0
           end
-          q << (pentagonal_coefs == false ? sign : sign * pentagonal_num_plus)
+          q << (pent_coefs == false ? sign : sign * pentagonal_num_plus)
           (min_next_pent - pentagonal_num_plus - 1).abs.times do
             q << 0
           end
