bfb906d20f05aee3e3162ff9fcb32c51d8c2d923
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-23 22:03:31 -0500

    feat: add KleinJInvariant module with klein_j_function with precision parameter

---
+++ lib/modular_forms/klein_j_invariant.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../numeric_helpers/numeric_helpers'
+require_relative 'eisenstein_series'
+require_relative 'dedekind_eta_functions'
+
+module ModularForms
+  # ModularForms::KleinJInvariant
+  #
+  # This module provides a generator for j function.
+  module KleinJInvariant
+    def self.klein_j_function(precision)
+      e4_cubed = EisensteinSeries.eisenstein_serie_power(4, 3, precision + 1)
+      delta_discriminant = DedekindEtaFunctions.eta_function_power(24, precision + 1)
+      NumericHelpers.quotient_of_series(e4_cubed, delta_discriminant, precision)
+    end
+  end
+end
