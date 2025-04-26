0837c2031d4d664c3ca2fb859ba8b379d5e5057a
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-26 00:31:52 -0500

    feat(HeckeOperators): add hecke_prime_cusp method with p, k, and precision parameter

--- lib/modular_forms/hecke_operators.rb
+++ lib/modular_forms/hecke_operators.rb
@@ -10,9 +10,9 @@ module ModularForms
     def self.hecke_prime_non_cusp(non_cusp_form, p, k, precision) # rubocop:disable Naming/MethodParameterName,Metrics/MethodLength,Metrics/AbcSize
       raise ArgumentError, 'p must be a prime number' unless NumericHelpers.prime_number?(p)
 
-      len = non_cusp_form.length
-      minimum_len = (precision - 1) * p + 1
-      raise ArgumentError, "size of q-expansion must be at least #{minimum_len}" if len < minimum_len
+      q_size = non_cusp_form.length
+      minimum_q_size = (precision - 1) * p + 1
+      raise ArgumentError, "size of q-expansion must be at least #{minimum_q_size}" if q_size < minimum_q_size
 
       normalization_factor = p**(k - 1)
       (0..precision - 1).map do |n|
@@ -20,6 +20,25 @@ module ModularForms
           non_cusp_form[p * n]
         else
           non_cusp_form[p * n] + normalization_factor * non_cusp_form[n / p]
+        end
+      end
+    end
+
+    def self.hecke_prime_cusp(cusp_form, p, k, precision) # rubocop:disable Naming/MethodParameterName,Metrics/MethodLength,Metrics/AbcSize
+      raise ArgumentError, 'p must be a prime number' unless NumericHelpers.prime_number?(p)
+
+      q_size = cusp_form.length
+      minimum_q_size = precision * p
+      raise ArgumentError, "size of q-expansion must be at least #{minimum_q_size}" if q_size < minimum_q_size
+
+      cusp_form.unshift(0)
+      normalization_factor = p**(k - 1)
+
+      (1..precision).map do |n|
+        if n % p != 0
+          cusp_form[p * n]
+        else
+          cusp_form[p * n] + normalization_factor * cusp_form[n / p]
         end
       end
     end
