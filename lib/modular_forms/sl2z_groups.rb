b687fd534e00db11570358fc492e99813db6dbe7
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-27 23:45:35 -0500

    feat: add SL2Zgroups module for SL_2(Z) matrices and related algebraic structures

---
+++ lib/modular_forms/sl2z_groups.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../numeric_helpers/numeric_helpers'
+
+module ModularForms
+  # ModularForms::SL2Zgroups
+  #
+  # This module provides matrices from SL_2(Z) and related algebraic structures.
+  module SL2Zgroups
+    T_MATRIX = [[1, 1], [0, 1]].freeze
+    S_MATRIX = [[0, -1], [1, 0]].freeze
+    NEGATIVE_S_MATRIX = [[0, 1], [-1, 0]].freeze
+    I_MATRIX = [[1, 0], [0, 1]].freeze
+    NEGATIVE_I_MATRIX = [[-1, 0], [0, -1]].freeze
+    ST_MATRIX = [[0, -1], [1, 1]].freeze
+    U_MATRIX = [[1, 0], [1, 1]].freeze
+  end
+end
