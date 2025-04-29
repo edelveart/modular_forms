032e1d2678b8d3bac18b5b0fac000cd820f3e14b
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-29 01:19:55 -0500

    refactor(KleinJInvariant): fix eta_function_pow method call

--- lib/modular_forms/klein_j_invariant.rb
+++ lib/modular_forms/klein_j_invariant.rb
@@ -7,11 +7,11 @@ require_relative 'dedekind_eta_functions'
 module ModularForms
   # ModularForms::KleinJInvariant
   #
-  # This module provides a generator for j function.
+  # This module provides an array of values for the j-invariant function.
   module KleinJInvariant
     def self.modular_j_function(precision)
       e4_cubed = EisensteinSeries.eisenstein_serie_power(4, 3, precision + 1)
-      delta_discriminant = DedekindEtaFunctions.eta_function_power(24, precision + 1)
+      delta_discriminant = DedekindEtaFunctions.eta_function_pow(24, precision + 1)
       NumericHelpers.quotient_of_series(e4_cubed, delta_discriminant, precision)
     end
   end
