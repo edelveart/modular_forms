e5ede996858786a75919bd17f26cb91bf266ef25
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-29 00:59:30 -0500

    feat(EllipticCurves): add module for computing points and arithmetic properties of elliptic curves

---
+++ lib/modular_forms/elliptic_curves.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../numeric_helpers/numeric_helpers'
+
+module ModularForms
+  # ModularForms::EllipticCurves
+  #
+  # This module provides methods for generating points of Elliptic Curves
+  module EllipticCurves
+    def self.point_on_curve?(point, a, b) # rubocop:disable Naming/MethodParameterName
+      x, y = point
+      y**2 == x**3 + x * a + b
+    end
+  end
+end
