e603b133ce0bdd7deeba3381b76d90c5350bf7fc
Edgar Delgado Vega <edelve91@gmail.com>
2025-04-29 19:18:47 -0500

    feat(EllipticCurves): add point_addition method for curves of the form y^2=x^3+ax+b

--- lib/modular_forms/elliptic_curves.rb
+++ lib/modular_forms/elliptic_curves.rb
@@ -5,7 +5,7 @@ require_relative '../numeric_helpers/numeric_helpers'
 module ModularForms
   # ModularForms::EllipticCurves
   #
-  # This module provides methods for generating points of Elliptic Curves
+  # This module provides methods for generating points on Elliptic Curves (short Weierstrass form) over char(k) !={2, 3}
   module EllipticCurves
     def self.point_on_curve?(point, a, b) # rubocop:disable Naming/MethodParameterName
       x, y = point
@@ -17,7 +17,22 @@ module ModularForms
     end
 
     def self.j_invariant(a, b) # rubocop:disable Naming/MethodParameterName
-      1728 * Rational(* 4 * a**3, 4 * a**3 + 27 * b**2)
+      1728 * Rational(4 * a**3, 4 * a**3 + 27 * b**2)
+    end
+
+    def self.point_addition(p, q, a) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Naming/MethodParameterName
+      x1, y1 = p
+      x2, y2 = q
+      return :infinity if x1 == x2 && y1 + y2 == 0 # rubocop:disable Style/NumericPredicate
+
+      if p != q # rubocop:disable Style/ConditionalAssignment
+        lambda_m = Rational(y2 - y1, x2 - x1)
+      else
+        lambda_m = Rational(3 * x1**2 + a, 2 * y1)
+      end
+      x3 = lambda_m**2 - x1 - x2
+      y3 = lambda_m * (x1 - x3) - y1
+      [x3, y3]
     end
   end
 end
