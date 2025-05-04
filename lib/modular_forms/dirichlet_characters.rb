f903c6ef585a5661a53a88ccd78292ada60449c1
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-03 23:35:17 -0500

    feat(DirichletCharacters): implement conrey_p_minus1(modp, a) method to replicate the behavior of $\chi_{P}(P-1,\cdot)$.

--- lib/modular_forms/dirichlet_characters.rb
+++ lib/modular_forms/dirichlet_characters.rb
@@ -10,5 +10,13 @@ module ModularForms
     def self.dirichlet_trivchar(modq, a) # rubocop:disable Naming/MethodParameterName
       NumericHelpers.gcd(a, modq) == 1 ? 1 : 0
     end
+
+    def self.conrey_p_pminus1(modp, a) # rubocop:disable Naming/MethodParameterName
+      raise "#{modp} must be a prime number" if !NumericHelpers.prime_number?(modp) # rubocop:disable Style/NegatedIf
+      return 0 if a % modp == 0 # rubocop:disable Style/NumericPredicate
+      return 1 if NumericHelpers.euler_criterion(a, modp)
+
+      -1 if !NumericHelpers.euler_criterion(a, modp) # rubocop:disable Style/NegatedIf
+    end
   end
 end
