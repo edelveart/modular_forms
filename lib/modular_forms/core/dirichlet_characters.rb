c8f27f163bab2e4609e23fbee0631623bf2e9de0
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-08 14:27:59 -0500

    feat(DirichletCharacters): add gauss_sum_conrey_p_minus1 method with parity (odd/even) parameter

--- lib/modular_forms/core/dirichlet_characters.rb
+++ lib/modular_forms/core/dirichlet_characters.rb
@@ -11,6 +11,11 @@ module ModularForms
     module DirichletCharacters
       PI2 = 2 * Math::PI
 
+      PARITY = {
+        EVEN: 'even',
+        ODD: 'odd'
+      }.freeze
+
       def self.dirichlet_trivchar(modq, a) # rubocop:disable Naming/MethodParameterName
         NumericHelpers.gcd(a, modq) == 1 ? 1 : 0
       end
@@ -28,11 +33,24 @@ module ModularForms
       end
 
       def self.gauss_sum_triv(q_char, a) # rubocop:disable Naming/MethodParameterName
-        t_a = 0
+        t_a = 0 + 0i
         (0..q_char - 1).each do |r|
           t_a += dirichlet_trivchar(q_char, r) * exponential(a, r, q_char)
         end
         t_a.real.to_f.round(2)
+      end
+
+      def self.gauss_sum_conrey_p_minus1(q_char, a, parity) # rubocop:disable Naming/MethodParameterName,Metrics/AbcSize
+        raise "Parity '#{parity}' must be either 'even' or 'odd'" if parity != PARITY[:ODD] && parity != PARITY[:EVEN]
+
+        t_a = 0 + 0i
+        (0..q_char - 1).each do |r|
+          t_a += conrey_p_pminus1(q_char, r) * exponential(a, r, q_char)
+        end
+
+        return t_a.real.round(10) if parity == PARITY[:EVEN]
+
+        Complex(0, t_a.imaginary.round(10)) if parity == PARITY[:ODD]
       end
     end
   end
