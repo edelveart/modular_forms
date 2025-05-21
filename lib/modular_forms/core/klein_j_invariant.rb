30f61036da1f4cf1f18272449093252bda35bf64
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-20 21:49:58 -0500

    fix(KleinJINvariant): rename eisenstein_series_pow method

--- lib/modular_forms/core/klein_j_invariant.rb
+++ lib/modular_forms/core/klein_j_invariant.rb
@@ -11,7 +11,7 @@ module ModularForms
     # This module provides an array of values for the j-invariant function.
     module KleinJInvariant
       def self.modular_j_function(precision)
-        e4_cubed = EisensteinSeries.eisenstein_serie_power(4, 3, precision + 1)
+        e4_cubed = EisensteinSeries.eisenstein_series_pow(4, 3, precision + 1)
         delta_discriminant = DedekindEtaFunctions.eta_function_pow(24, precision + 1)
         NumericHelpers.quotient_of_series(e4_cubed, delta_discriminant, precision)
       end
